3d9d71d61ead1439dcdf8ed9e2ed408a02044b33
Denis Badurina <badurinadenis@gmail.com>
2022-09-21 19:26:03 +0200

    Stabilise and improve tests (#133)

--- packages/server/test/node.spec.ts
+++ packages/server/test/node.spec.ts
@@ -1,34 +1,18 @@
 import { createServerAdapter } from '@whatwg-node/server';
-import { createServer, Server } from 'http';
 import { fetch, Response } from '@whatwg-node/fetch';
+import { startTServer } from './tserver';
 
 describe('Node Specific Cases', () => {
-  let port = 9876;
-  let server: Server | undefined;
-  afterEach(done => {
-    if (server) {
-      server.close(err => {
-        if (err) {
-          throw err;
-        }
-        server = undefined;
-        done();
-      });
-    } else {
-      done();
-    }
-    port = Math.floor(Math.random() * 1000) + 9800;
-  });
   it('should handle empty responses', async () => {
     const serverAdapter = createServerAdapter(() => {
       return undefined as any;
     });
-    server = createServer(serverAdapter);
-    await new Promise<void>(resolve => server!.listen(port, resolve));
-    const response = await fetch(`http://localhost:${port}`);
+    const { url } = startTServer(serverAdapter);
+    const response = await fetch(url);
     await response.text();
     expect(response.status).toBe(404);
   });
+
   it('should handle waitUntil properly', async () => {
     let flag = false;
     const serverAdapter = createServerAdapter((_request, { waitUntil }) => {
@@ -43,9 +27,8 @@ describe('Node Specific Cases', () => {
         })
       );
     });
-    server = createServer(serverAdapter);
-    await new Promise<void>(resolve => server!.listen(port, resolve));
-    const response$ = fetch(`http://localhost:${port}`);
+    const { url } = startTServer(serverAdapter);
+    const response$ = fetch(url);
     expect(flag).toBe(false);
     const response = await response$;
     await response.text();
