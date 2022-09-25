b1c18dcb42a7a86e074ceb81a346225a32732e7c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-09-26 02:06:21 +0300

    Fix FormData and add tests

---
+++ packages/server/test/formdata.spec.ts
@@ -0,0 +1,46 @@
+import { createFetch } from "@whatwg-node/fetch";
+import { createServerAdapter } from "@whatwg-node/server";
+import { createServer, Server } from "http"
+
+describe('FormData', () => {
+    let server: Server;
+    let url: string;
+    afterEach(done => {
+        server.close(done);
+    });
+    ['fieldsFirst:true', 'fieldsFirst:false'].forEach((fieldsFirstFlag) => {
+        const fetchAPI = createFetch({
+            formDataLimits: {
+                fieldsFirst: fieldsFirstFlag === 'fieldsFirst:true',
+            },
+        });
+
+        it('should forward formdata correctly', async () => {
+            const requestHandler = jest.fn().mockImplementation(() => new fetchAPI.Response(null, {
+                status: 204
+            }));
+            const adapter = createServerAdapter(requestHandler, fetchAPI.Request);
+            server = createServer(adapter);
+            await new Promise<void>(resolve => {
+                server.listen(0, () => {
+                    url = `http://localhost:${(server.address() as any).port}`;
+                    resolve();
+                });
+            });
+            const formData = new fetchAPI.FormData();
+            formData.append('foo', 'bar');
+            formData.append('baz', new fetchAPI.File(['baz'], 'baz.txt'));
+            const response = await fetchAPI.fetch(url, {
+                method: 'POST',
+                body: formData
+            });
+            await response.text();
+            const request = requestHandler.mock.calls[0][0];
+            const body = await request.formData();
+            expect(body.get('foo')).toBe('bar');
+            const file = body.get('baz') as File;
+            expect(file.name).toBe('baz.txt');
+            expect(await file.text()).toBe('baz');
+        })
+    })
+})
