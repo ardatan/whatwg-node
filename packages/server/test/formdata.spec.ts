1ebbc300015f4baca60308b0d2e4f72a5d68065c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-09-26 02:23:45 +0300

    Better test grouping

--- packages/server/test/formdata.spec.ts
+++ packages/server/test/formdata.spec.ts
@@ -15,32 +15,35 @@ describe('FormData', () => {
             },
         });
 
-        it('should forward formdata correctly', async () => {
-            const requestHandler = jest.fn().mockImplementation(() => new fetchAPI.Response(null, {
-                status: 204
-            }));
-            const adapter = createServerAdapter(requestHandler, fetchAPI.Request);
-            server = createServer(adapter);
-            await new Promise<void>(resolve => {
-                server.listen(0, () => {
-                    url = `http://localhost:${(server.address() as any).port}`;
-                    resolve();
+        describe(fieldsFirstFlag, () => {
+            it('should forward formdata correctly', async () => {
+                const requestHandler = jest.fn().mockImplementation(() => new fetchAPI.Response(null, {
+                    status: 204
+                }));
+                const adapter = createServerAdapter(requestHandler, fetchAPI.Request);
+                server = createServer(adapter);
+                await new Promise<void>(resolve => {
+                    server.listen(0, () => {
+                        url = `http://localhost:${(server.address() as any).port}`;
+                        resolve();
+                    });
                 });
-            });
-            const formData = new fetchAPI.FormData();
-            formData.append('foo', 'bar');
-            formData.append('baz', new fetchAPI.File(['baz'], 'baz.txt'));
-            const response = await fetchAPI.fetch(url, {
-                method: 'POST',
-                body: formData
-            });
-            await response.text();
-            const request = requestHandler.mock.calls[0][0];
-            const body = await request.formData();
-            expect(body.get('foo')).toBe('bar');
-            const file = body.get('baz') as File;
-            expect(file.name).toBe('baz.txt');
-            expect(await file.text()).toBe('baz');
+                const formData = new fetchAPI.FormData();
+                formData.append('foo', 'bar');
+                formData.append('baz', new fetchAPI.File(['baz'], 'baz.txt', { type: 'text/plain' }));
+                const response = await fetchAPI.fetch(url, {
+                    method: 'POST',
+                    body: formData
+                });
+                await response.text();
+                const request = requestHandler.mock.calls[0][0];
+                const body = await request.formData();
+                expect(body.get('foo')).toBe('bar');
+                const file = body.get('baz') as File;
+                expect(file.name).toBe('baz.txt');
+                expect(file.type).toBe('text/plain');
+                expect(await file.text()).toBe('baz');
+            })
         })
     })
 })
