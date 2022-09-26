596f6464ba00d85301e694573c799caeb3b259bd
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-09-26 04:43:40 +0300

    ..

--- packages/server/test/formdata.spec.ts
+++ packages/server/test/formdata.spec.ts
@@ -3,48 +3,62 @@ import { createServerAdapter } from '@whatwg-node/server';
 import { createServer, Server } from 'http';
 
 describe('FormData', () => {
-  let server: Server;
-  let url: string;
-  afterEach(done => {
-    server.close(done);
-  });
-  ['fieldsFirst:true', 'fieldsFirst:false'].forEach(fieldsFirstFlag => {
-    const fetchAPI = createFetch({
-      formDataLimits: {
-        fieldsFirst: fieldsFirstFlag === 'fieldsFirst:true',
-      },
+    let server: Server;
+    let url: string;
+    afterEach(done => {
+        server.close(done);
     });
+    ['fieldsFirst:true', 'fieldsFirst:false'].forEach(fieldsFirstFlag => {
+        const fetchAPI = createFetch({
+            formDataLimits: {
+                fieldsFirst: fieldsFirstFlag === 'fieldsFirst:true',
+            },
+        });
 
-    describe(fieldsFirstFlag, () => {
-      it('should forward formdata correctly', async () => {
-        expect.assertions(4);
-        const adapter = createServerAdapter(async request => {
-          const body = await request.formData();
-          expect(body.get('foo')).toBe('bar');
-          const file = body.get('baz') as File;
-          expect(file.name).toBe('baz.txt');
-          expect(file.type).toBe('text/plain');
-          expect(await file.text()).toBe('baz');
-          return new fetchAPI.Response(null, {
-            status: 204,
-          });
-        }, fetchAPI.Request);
-        server = createServer(adapter);
-        await new Promise<void>(resolve => {
-          server.listen(0, () => {
-            url = `http://localhost:${(server.address() as any).port}`;
-            resolve();
-          });
+        describe(fieldsFirstFlag, () => {
+            it('should forward formdata correctly', async () => {
+                let receivedFieldContent: string | undefined;
+                let receivedFileName: string | undefined;
+                let receivedFileType: string | undefined;
+                let receivedFileContent: string | undefined;
+                const adapter = createServerAdapter(async request => {
+                    try {
+                        const body = await request.formData();
+                        receivedFieldContent = body.get('foo') as string
+                        const file = body.get('baz') as File;
+                        receivedFileName = file.name;
+                        receivedFileType = file.type;
+                        receivedFileContent = await file.text();
+                    } catch (e: any) {
+                        return new Response(e.stack, {
+                            status: 500,
+                        });
+                    }
+                    return new fetchAPI.Response(null, {
+                        status: 204,
+                    });
+                }, fetchAPI.Request);
+                server = createServer(adapter);
+                await new Promise<void>(resolve => {
+                    server.listen(0, () => {
+                        url = `http://localhost:${(server.address() as any).port}`;
+                        resolve();
+                    });
+                });
+                const formData = new fetchAPI.FormData();
+                formData.append('foo', 'bar');
+                formData.append('baz', new fetchAPI.File(['baz'], 'baz.txt', { type: 'text/plain' }));
+                const response = await fetchAPI.fetch(url, {
+                    method: 'POST',
+                    body: formData,
+                });
+                console.log(await response.text());
+                expect(response.status).toBe(204);
+                expect(receivedFieldContent).toBe('bar');
+                expect(receivedFileName).toBe('baz.txt');
+                expect(receivedFileType).toBe('text/plain');
+                expect(receivedFileContent).toBe('baz');
+            });
         });
-        const formData = new fetchAPI.FormData();
-        formData.append('foo', 'bar');
-        formData.append('baz', new fetchAPI.File(['baz'], 'baz.txt', { type: 'text/plain' }));
-        const response = await fetchAPI.fetch(url, {
-          method: 'POST',
-          body: formData,
-        });
-        await response.text();
-      });
     });
-  });
 });
