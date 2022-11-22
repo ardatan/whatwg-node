eb29958feaa1ae33a3ca423136a5a91ad7cb6e4c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-11-22 12:42:45 +0300

    Prettier

--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -1,5 +1,5 @@
 export interface FetchEvent extends Event {
-    waitUntil(f: Promise<any>): void;
-    request: Request;
-    respondWith(r: Response | PromiseLike<Response>): void;
+  waitUntil(f: Promise<any>): void;
+  request: Request;
+  respondWith(r: Response | PromiseLike<Response>): void;
 }
