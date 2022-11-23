de8afa87eb260dba5eda8ed3c4e1fd371036e843
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-11-23 15:17:49 +0300

    Go

--- e2e/bun/tests/bun.spec.ts
+++ e2e/bun/tests/bun.spec.ts
@@ -1,29 +1,29 @@
 import { createTestServerAdapter } from '@e2e/shared-server';
-import { describe, it } from 'bun:test'
-import { Server } from 'bun'
+import { describe, it } from 'bun:test';
+import { Server } from 'bun';
 import { assertDeployedEndpoint } from '@e2e/shared-scripts';
 
-let server: Server
-let url: string
+let server: Server;
+let url: string;
 function beforeEach() {
-    server = Bun.serve({
-        fetch: createTestServerAdapter(),
-        port: 3000,
-    })
-    url = `http://${server.hostname}:${server.port}`
+  server = Bun.serve({
+    fetch: createTestServerAdapter(),
+    port: 3000,
+  });
+  url = `http://${server.hostname}:${server.port}`;
 }
 
 function afterEach() {
-    server.stop()
+  server.stop();
 }
 
 describe('Bun', () => {
-    it('works', async () => {
-        beforeEach();
-        try {
-            await assertDeployedEndpoint(url);
-        } finally {
-            afterEach();
-        }
-    })
-})
+  it('works', async () => {
+    beforeEach();
+    try {
+      await assertDeployedEndpoint(url);
+    } finally {
+      afterEach();
+    }
+  });
+});
