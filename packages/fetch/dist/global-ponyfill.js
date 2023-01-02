e5014cff99fca6e398535c2a75e3f89dae86fcfc
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-01-02 11:35:54 +0300

    Ponyfill URLPattern

--- packages/fetch/dist/global-ponyfill.js
+++ packages/fetch/dist/global-ponyfill.js
@@ -14,4 +14,8 @@ module.exports.btoa = globalThis.btoa;
 module.exports.TextEncoder = globalThis.TextEncoder;
 module.exports.TextDecoder = globalThis.TextDecoder;
 module.exports.URLPattern = globalThis.URLPattern;
+if (!module.exports.URLPattern) {
+    const urlPatternModule = require('urlpattern-polyfill');
+    module.exports.URLPattern = urlPatternModule.URLPattern;
+}
 module.exports.createFetch = () => globalThis;
