7382018c7c623ff2df9494bd56a9099642eefe55
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-02-21 15:01:08 +0300

    Add tests for useErrorHandling

---
+++ packages/server/test/useErrorHandling.spec.ts
@@ -0,0 +1,20 @@
+import { createServerAdapter } from '../src/createServerAdapter';
+import { useErrorHandling } from '../src/plugins/useErrorHandling';
+
+describe('useErrorHandling', () => {
+  it('should return 500 when error is thrown', async () => {
+    const router = createServerAdapter(
+      () => {
+        throw new Error('Unexpected error');
+      },
+      {
+        plugins: [useErrorHandling()],
+      },
+    );
+    const response = await router.fetch('http://localhost/greetings/John');
+    expect(response.status).toBe(500);
+    expect(response.statusText).toBe('Internal Server Error');
+    const text = await response.text();
+    expect(text).toContain('Error: Unexpected error');
+  });
+});
