69eff9e098b3e63673a6cd7202570ec9c7fe67c0
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-05-15 15:18:19 +0300

    Go

--- packages/events/dist/node-ponyfill.js
+++ packages/events/dist/node-ponyfill.js
@@ -1,6 +1,6 @@
 module.exports.CustomEvent = globalThis.CustomEvent;
 if (!module.exports.CustomEvent) {
-  module.exports.CustomEvent = class CustomEvent extends module.exports.Event {
+  module.exports.CustomEvent = class CustomEvent extends Event {
     constructor(type, options) {
       super(type, options);
       this.detail = options && options.detail;
