552afdd156de29d1f0bb3f2d172e3189c4dd7c6f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-05-25 13:16:12 +0300

    Changeset and tests

---
+++ .changeset/nine-buttons-applaud.md
@@ -0,0 +1,5 @@
+---
+'@whatwg-node/events': patch
+---
+
+Follow the spec and set `detail` to null by default
--- packages/events/tests/events.spec.ts
+++ packages/events/tests/events.spec.ts
@@ -1,11 +1,18 @@
 import { CustomEvent } from '@whatwg-node/events';
 
 describe('CustomEvent', () => {
-  it('detail', () => {
+  it('detail should be set', () => {
     const target = new EventTarget();
     const listener = jest.fn();
     target.addEventListener('test', listener);
     target.dispatchEvent(new CustomEvent('test', { detail: 123 }));
     expect(listener).toHaveBeenCalledWith(expect.objectContaining({ detail: 123 }));
   });
+  it('detail should be null by default', () => {
+    const target = new EventTarget();
+    const listener = jest.fn();
+    target.addEventListener('test', listener);
+    target.dispatchEvent(new CustomEvent('test'));
+    expect(listener).toHaveBeenCalledWith(expect.objectContaining({ detail: null }));
+  })
 });
