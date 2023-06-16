439c10e25bcd7777f7de8fe408912a0670c0f598
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-06-16 13:29:00 +0300

    Go

---
+++ packages/cookie-store/src/getCookieString.ts
@@ -0,0 +1,37 @@
+import { Cookie, CookieListItem } from './types';
+
+export function getCookieString(item: CookieListItem | Cookie) {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  let cookieString = `${item.name}=${encodeURIComponent(item.value!)}`;
+
+  if (item.domain) {
+    cookieString += '; Domain=' + item.domain;
+  }
+
+  if (item.path) {
+    cookieString += '; Path=' + item.path;
+  }
+
+  if (typeof item.expires === 'number') {
+    cookieString += '; Expires=' + new Date(item.expires).toUTCString();
+  }
+
+  if ((item.name && item.name.startsWith('__Secure')) || item.secure) {
+    item.sameSite = 'lax';
+    cookieString += '; Secure';
+  }
+
+  switch (item.sameSite) {
+    case 'lax':
+      cookieString += '; SameSite=Lax';
+      break;
+    case 'strict':
+      cookieString += '; SameSite=Strict';
+      break;
+    case 'none':
+      cookieString += '; SameSite=None';
+      break;
+  }
+
+  return cookieString;
+}
