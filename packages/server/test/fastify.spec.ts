7409faf6e082630a8041337eef0a25e21f51dc8e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-07-14 18:06:35 +0300

    Go

--- packages/server/test/fastify.spec.ts
+++ packages/server/test/fastify.spec.ts
@@ -1,17 +1,8 @@
-import { AddressInfo, Socket } from 'net';
-import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
-import { fetch, ReadableStream, Response, TextEncoder, URL } from '@whatwg-node/fetch';
+import fastify, { FastifyReply, FastifyRequest } from 'fastify';
+import { ReadableStream, Response, TextEncoder, URL } from '@whatwg-node/fetch';
 import { createServerAdapter } from '../src/createServerAdapter.js';
 
 describe('Fastify', () => {
-  let fastifyServer: FastifyInstance;
-  const connections = new Set<Socket>();
-  afterEach(() => {
-    connections.forEach(socket => {
-      socket.destroy();
-    });
-    return fastifyServer?.close();
-  });
   it('should handle streams', async () => {
     let cnt = 0;
     const encoder = new TextEncoder();
@@ -39,13 +30,7 @@ describe('Fastify', () => {
           }),
         ),
     );
-    fastifyServer = fastify();
-    fastifyServer.server.on('connection', socket => {
-      connections.add(socket);
-      socket.on('close', () => {
-        connections.delete(socket);
-      });
-    });
+    const fastifyServer = fastify();
     fastifyServer.route({
       url: '/mypath',
       method: ['GET', 'POST', 'OPTIONS'],
@@ -65,14 +50,10 @@ describe('Fastify', () => {
         return reply;
       },
     });
-    await fastifyServer.listen({
-      port: 0,
+    const res = await fastifyServer.inject({
+      url: '/mypath',
     });
-    const res = await fetch(
-      `http://localhost:${(fastifyServer.server.address() as AddressInfo).port}/mypath`,
-    );
-    const body = await res.text();
-    expect(body).toMatchInlineSnapshot(`
+    expect(res.body).toMatchInlineSnapshot(`
       "{"cnt":0}
       {"cnt":1}
       {"cnt":2}
@@ -86,13 +67,7 @@ describe('Fastify', () => {
       const searchParamsObj = Object.fromEntries(url.searchParams);
       return Response.json(searchParamsObj);
     });
-    fastifyServer = fastify();
-    fastifyServer.server.on('connection', socket => {
-      connections.add(socket);
-      socket.on('close', () => {
-        connections.delete(socket);
-      });
-    });
+    const fastifyServer = fastify();
     fastifyServer.route({
       url: '/mypath',
       method: ['GET', 'POST', 'OPTIONS'],
@@ -112,15 +87,10 @@ describe('Fastify', () => {
         return reply;
       },
     });
-    await fastifyServer.listen({
-      port: 0,
+    const res = await fastifyServer.inject({
+      url: '/mypath?foo=bar&baz=qux',
     });
-    const res = await fetch(
-      `http://localhost:${
-        (fastifyServer.server.address() as AddressInfo).port
-      }/mypath?foo=bar&baz=qux`,
-    );
-    const body = await res.json();
+    const body = res.json();
     expect(body).toMatchObject({
       foo: 'bar',
       baz: 'qux',
