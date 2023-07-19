9d90ef193b244a3bd26099d501defd6034fc86eb
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-07-19 06:30:13 -0400

    enhance(node-fetch): use `POSTFIELDS` instead of `UPLOAD` for static results in `fetchCurl` (#702)

--- packages/node-fetch/src/fetchCurl.ts
+++ packages/node-fetch/src/fetchCurl.ts
@@ -32,17 +32,21 @@ export function fetchCurl<TResponseJSON = any, TRequestJSON = any>(
       : 0;
   });
 
-  const nodeReadable = (
-    fetchRequest.body != null
-      ? 'pipe' in fetchRequest.body
-        ? fetchRequest.body
-        : Readable.from(fetchRequest.body)
-      : null
-  ) as Readable | null;
+  if (fetchRequest['bodyType'] === 'String') {
+    curlHandle.setOpt('POSTFIELDS', fetchRequest['bodyInit'] as string);
+  } else {
+    const nodeReadable = (
+      fetchRequest.body != null
+        ? 'pipe' in fetchRequest.body
+          ? fetchRequest.body
+          : Readable.from(fetchRequest.body)
+        : null
+    ) as Readable | null;
 
-  if (nodeReadable) {
-    curlHandle.setOpt('UPLOAD', true);
-    curlHandle.setUploadStream(nodeReadable);
+    if (nodeReadable) {
+      curlHandle.setOpt('UPLOAD', true);
+      curlHandle.setUploadStream(nodeReadable);
+    }
   }
 
   if (process.env.DEBUG) {
