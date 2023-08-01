4a74acc2683372904ac217c699ce0540c62584c6
Alexey Lyakhov <justfly1984@gmail.com>
2023-08-01 23:18:51 +0700

    fix benchmark

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -12,6 +12,12 @@ const serverAdapter = createServerAdapter(async req => {
   return Response.json({ message: `Hello, ${name}!` });
 });
 
+// @ts-ignore Type 'ServerAdapter<{}, ServerAdapterBaseObject<{}, (req: Request) => Promise<Response>>>' is not assignable to type 'RequestListener<typeof IncomingMessage, typeof ServerResponse>'.
+// Types of parameters 'req' and 'req' are incompatible.
+// Type 'IncomingMessage' is not assignable to type 'NodeRequest' with 'exactOptionalPropertyTypes: true'. Consider adding 'undefined' to the types of the target's properties.
+//   Types of property 'url' are incompatible.
+//     Type 'string | undefined' is not assignable to type 'string'.
+//       Type 'undefined' is not assignable to type 'string'.
 createServer(serverAdapter).listen(4000, () => {
   console.log('listening on 0.0.0.0:4000');
 });
