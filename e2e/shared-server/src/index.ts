8b00b486d3ba70efe736c6bfeeb9eb1ab9fa70d7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-12-01 17:06:39 +0300

    Simplify e2e setup

--- e2e/shared-server/src/index.ts
+++ e2e/shared-server/src/index.ts
@@ -1,17 +1,12 @@
 import { Response } from '@whatwg-node/fetch';
 import { createServerAdapter } from '@whatwg-node/server';
 
-export function createTestServerAdapter<TServerContext = {}>() {
-  return createServerAdapter<TServerContext>(async req => {
-    const headers: Record<string, string> = {};
-    req.headers.forEach((value, key) => {
-      headers[key] = value;
-    });
-    return Response.json({
+export const createTestServerAdapter = <TServerContext = {}>() =>
+  createServerAdapter<TServerContext>(async req =>
+    Response.json({
       url: req.url,
       method: req.method,
-      headers,
+      headers: Object.fromEntries(req.headers.entries()),
       reqText: req.method === 'POST' ? await req.text() : '',
-    });
-  });
-}
+    }),
+  );
