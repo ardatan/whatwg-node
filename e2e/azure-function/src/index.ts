3de18a43c9fb7c23af0d07c416ed4eff6b6d093d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-01-23 15:22:51 +0300

    Update Azure example

--- e2e/azure-function/src/index.ts
+++ e2e/azure-function/src/index.ts
@@ -1,3 +1,15 @@
+import { app, InvocationContext } from '@azure/functions';
 import { createTestServerAdapter } from '@e2e/shared-server';
 
-export default createTestServerAdapter();
+const handler = createTestServerAdapter<InvocationContext>();
+
+declare global {
+  interface ReadableStream {
+    [Symbol.asyncIterator](): AsyncIterableIterator<Uint8Array>;
+  }
+}
+
+app.http('whatwgnode', {
+  methods: ['GET', 'POST'],
+  handler,
+});
