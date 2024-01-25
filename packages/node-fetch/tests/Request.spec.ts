164af000d1a8795213568269ffb51cd4b7cea27b
Daniel Choudhury <dannychoudhury@gmail.com>
2024-01-25 17:22:50 +0700

    Add more expects

--- packages/node-fetch/tests/Request.spec.ts
+++ packages/node-fetch/tests/Request.spec.ts
@@ -6,22 +6,29 @@ describe('Request', () => {
     expect(req.method).toBe('GET');
   });
 
-  it('should instatitate PonyfillRequest from a Request correctly', () => {
-    const req = new Request('http://a', { method: 'put', headers: { 'x-test': '1' } });
+  it('should instatitate PonyfillRequest from a Request correctly', async () => {
+    const req = new Request('http://a', {
+      method: 'put',
+      headers: { 'x-test': '1' },
+      body: 'test',
+    });
 
     const ponyReq = new PonyfillRequest(req);
     expect(ponyReq.method).toBe('PUT');
     expect(ponyReq.headers.get('x-test')).toBe('1');
+    expect(await ponyReq.text()).toBe('test');
   });
 
-  it('should instatitate PonyfillRequest from another PonyfillRequest correctly', () => {
+  it('should instatitate PonyfillRequest from another PonyfillRequest correctly', async () => {
     const firstPony = new PonyfillRequest('http://a', {
       method: 'put',
       headers: { 'x-test': '1' },
+      body: 'test',
     });
 
     const secondPony = new PonyfillRequest(firstPony);
     expect(secondPony.method).toBe('PUT');
     expect(secondPony.headers.get('x-test')).toBe('1');
+    expect(await secondPony.text()).toBe('test');
   });
 });
