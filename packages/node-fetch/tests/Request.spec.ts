e7ef8470e440a4592624bd512037f85dba3e97de
Daniel Choudhury <dannychoudhury@gmail.com>
2024-01-25 17:14:22 +0700

    Add test for instantiating PonyfillReq from another Req

--- packages/node-fetch/tests/Request.spec.ts
+++ packages/node-fetch/tests/Request.spec.ts
@@ -5,4 +5,23 @@ describe('Request', () => {
     const req = new PonyfillRequest('http://a', { method: 'get' });
     expect(req.method).toBe('GET');
   });
+
+  it('should instatitate PonyfillRequest from a Request correctly', () => {
+    const req = new Request('http://a', { method: 'put', headers: { 'x-test': '1' } });
+
+    const ponyReq = new PonyfillRequest(req);
+    expect(ponyReq.method).toBe('PUT');
+    expect(ponyReq.headers.get('x-test')).toBe('1');
+  });
+
+  it('should instatitate PonyfillRequest from another PonyfillRequest correctly', () => {
+    const firstPony = new PonyfillRequest('http://a', {
+      method: 'put',
+      headers: { 'x-test': '1' },
+    });
+
+    const secondPony = new PonyfillRequest(firstPony);
+    expect(secondPony.method).toBe('PUT');
+    expect(secondPony.headers.get('x-test')).toBe('1');
+  });
 });
