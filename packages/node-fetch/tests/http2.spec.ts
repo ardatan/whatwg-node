2c4647cea0dcca5d7a1581c159b7ace9a5023b5b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-08-22 12:40:44 +0300

    Fix HTTP2 tests with Curl

--- packages/node-fetch/tests/http2.spec.ts
+++ packages/node-fetch/tests/http2.spec.ts
@@ -1,5 +1,8 @@
+import { promises as fsPromises } from 'fs';
 import { createSecureServer, type Http2SecureServer } from 'http2';
 import { AddressInfo } from 'net';
+import { tmpdir } from 'os';
+import { join } from 'path';
 import { CertificateCreationResult, createCertificate } from 'pem';
 import { fetchPonyfill } from '../src/fetch';
 
@@ -9,6 +12,7 @@ describe('http2', () => {
     return;
   }
   let server: Http2SecureServer;
+  const oldEnvVar = process.env.NODE_EXTRA_CA_CERTS;
   beforeAll(async () => {
     const keys = await new Promise<CertificateCreationResult>((resolve, reject) => {
       createCertificate(
@@ -24,6 +28,9 @@ describe('http2', () => {
         },
       );
     });
+    const pemPath = join(tmpdir(), 'test.pem');
+    process.env.NODE_EXTRA_CA_CERTS = pemPath;
+    await fsPromises.writeFile(pemPath, keys.certificate);
     // Create a secure HTTP/2 server
     server = createSecureServer(
       {
@@ -42,6 +49,7 @@ describe('http2', () => {
     await new Promise<void>(resolve => server.listen(0, resolve));
   });
   afterAll(() => {
+    process.env.NODE_EXTRA_CA_CERTS = oldEnvVar;
     server.close();
   });
   it('works', async () => {
