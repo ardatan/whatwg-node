71c79c639713ef738bd63f233ec20bdc1181b8e5
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-08-22 13:01:09 +0300

    enhance(node-fetch): respect Node SSL-related env vars (#1592)

--- packages/node-fetch/tests/http2.spec.ts
+++ packages/node-fetch/tests/http2.spec.ts
@@ -1,5 +1,8 @@
-import { createSecureServer, type Http2SecureServer } from 'http2';
+import { promises as fsPromises } from 'fs';
+import { createSecureServer, ServerHttp2Session, type Http2SecureServer } from 'http2';
 import { AddressInfo } from 'net';
+import { tmpdir } from 'os';
+import { join } from 'path';
 import { CertificateCreationResult, createCertificate } from 'pem';
 import { fetchPonyfill } from '../src/fetch';
 
@@ -9,6 +12,9 @@ describe('http2', () => {
     return;
   }
   let server: Http2SecureServer;
+  let pemPath: string;
+  const oldEnvVar = process.env.NODE_EXTRA_CA_CERTS;
+  const sessions = new Set<ServerHttp2Session>();
   beforeAll(async () => {
     const keys = await new Promise<CertificateCreationResult>((resolve, reject) => {
       createCertificate(
@@ -24,6 +30,9 @@ describe('http2', () => {
         },
       );
     });
+    pemPath = join(tmpdir(), 'test.pem');
+    process.env.NODE_EXTRA_CA_CERTS = pemPath;
+    await fsPromises.writeFile(pemPath, keys.certificate);
     // Create a secure HTTP/2 server
     server = createSecureServer(
       {
@@ -39,10 +48,19 @@ describe('http2', () => {
       },
     );
 
+    server.on('session', session => {
+      sessions.add(session);
+    });
+
     await new Promise<void>(resolve => server.listen(0, resolve));
   });
-  afterAll(() => {
-    server.close();
+  afterAll(async () => {
+    await fsPromises.unlink(pemPath);
+    process.env.NODE_EXTRA_CA_CERTS = oldEnvVar;
+    for (const session of sessions) {
+      session.destroy();
+    }
+    await new Promise(resolve => server.close(resolve));
   });
   it('works', async () => {
     const res = await fetchPonyfill(`https://localhost:${(server.address() as AddressInfo).port}`, {
