677b7cee4a8b32771faeb05388fe94aa2c86a4a3
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-11-13 18:35:17 +0300

    Add tests for defineProperty

--- packages/server/test/utils.spec.ts
+++ packages/server/test/utils.spec.ts
@@ -8,4 +8,11 @@ describe('isolateObject', () => {
     a.a = 1;
     expect(b.a).toEqual(undefined);
   });
+  test('Object.create does not share property assignments with defineProperty', () => {
+    const origin = isolateObject({});
+    const a = Object.create(origin);
+    const b = Object.create(origin);
+    Object.defineProperty(a, 'a', { value: 1 });
+    expect(b.a).toEqual(undefined);
+  });
 });
