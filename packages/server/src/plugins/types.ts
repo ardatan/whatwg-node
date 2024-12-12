95e5ce4a32e51b8727b31ea711903e7924c8e47e
Valentin Cocaud <v.cocaud@gmail.com>
2024-12-12 15:03:21 +0100

    docs: add TSDoc to plugin's hooks (#1880)
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- packages/server/src/plugins/types.ts
+++ packages/server/src/plugins/types.ts
@@ -5,9 +5,36 @@ import {
 } from '../types.js';
 
 export interface ServerAdapterPlugin<TServerContext = {}> {
+  /**
+   * This hook is invoked for ANY incoming HTTP request. Here you can manipulate the request,
+   * create a short circuit before the request handler takes it over.
+   *
+   * Warning: Exceptions thrown by this hook are not caught.
+   * This means they will buble up to the HTTP server underlying implementation.
+   * For example, the `node:http` server crashes the entire process on uncaught exceptions.
+   */
   onRequest?: OnRequestHook<TServerContext & ServerAdapterInitialContext>;
+  /**
+   * This hook is invoked after a HTTP request (both GraphQL and NON GraphQL) has been processed
+   * and after the response has been forwarded to the client. Here you can perform any cleanup
+   * or logging operations, or you can manipulate the outgoing response object.
+   *
+   * Warning: Exceptions thrown by this hook are not caught.
+   * This means they will buble up to the HTTP server underlying implementation.
+   * For example, the `node:http` server crashes the entire process on uncaught exceptions.
+   */
   onResponse?: OnResponseHook<TServerContext & ServerAdapterInitialContext>;
+  /**
+   * This hook is invoked when the server is being disposed.
+   * The server disposal is triggered either by the request termination or the explicit server disposal.
+   * @see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-2.html
+   */
   [Symbol.dispose]?: () => void;
+  /**
+   * This hook is invoked when the server is being disposed.
+   * The server disposal is triggered either by the request termination or the explicit server disposal.
+   * @see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-2.html
+   */
   [Symbol.asyncDispose]?: () => PromiseLike<void> | void;
 }
 export type OnRequestHook<TServerContext> = (
