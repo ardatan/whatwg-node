6a1e807e482f9316c8e41405dfe71e1afb4ef63b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-02-14 14:06:05 +0300

    Make deno happy

--- packages/server/test/plugins.spec.ts
+++ packages/server/test/plugins.spec.ts
@@ -1,21 +1,31 @@
-import { describe, expect, it, jest } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import { Request, Response } from '@whatwg-node/fetch';
-import { createServerAdapter, OnResponseEventPayload } from '@whatwg-node/server';
+import { createServerAdapter } from '@whatwg-node/server';
 
 describe('Plugins', () => {
   it('should reflect updated response in subsequent plugins', async () => {
-    const firstOnResponse = jest.fn((payload: OnResponseEventPayload<{}>) => {
-      payload.setResponse(Response.json({ message: 'Good bye!' }, { status: 418 }));
-    });
-    const secondOnResponse = jest.fn((_payload: OnResponseEventPayload<{}>) => {});
+    let firstRes: Response | undefined;
+    let secondRes: Response | undefined;
     const adapter = createServerAdapter<{}>(() => Response.json({ message: 'Hello, World!' }), {
-      plugins: [{ onResponse: firstOnResponse }, { onResponse: secondOnResponse }],
+      plugins: [
+        {
+          onResponse({ response, setResponse }) {
+            firstRes = response;
+            setResponse(Response.json({ message: 'Good bye!' }, { status: 418 }));
+          },
+        },
+        {
+          onResponse({ response }) {
+            secondRes = response;
+          },
+        },
+      ],
     });
     const request = new Request('http://localhost');
     const response = await adapter.fetch(request);
     expect(response.status).toBe(418);
     expect(await response.json()).toEqual({ message: 'Good bye!' });
-    expect(firstOnResponse.mock.calls[0][0].response.status).toBe(200);
-    expect(secondOnResponse.mock.calls[0][0].response.status).toBe(418);
+    expect(firstRes?.status).toBe(200);
+    expect(secondRes?.status).toBe(418);
   });
 });
