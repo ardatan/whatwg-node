f966275d0e70c0f020cb817cabfdfdd64f2f1367
Valentin Cocaud <v.cocaud@gmail.com>
2025-03-08 14:29:42 +0100

    implement jest.clearAllMocks

--- deno-jest.ts
+++ deno-jest.ts
@@ -11,8 +11,17 @@ export {
 } from 'jsr:@std/testing/bdd';
 export { expect } from 'jsr:@std/expect';
 
+const mocks: { mockClear: () => void }[] = [];
+
 export const jest = {
-  fn,
+  fn<T extends (...args: any[]) => any>(implementation?: T) {
+    const f = fn(implementation);
+    mocks.push(f);
+    return f;
+  },
+  clearAllMocks: () => {
+    mocks.forEach(mock => mock.mockClear());
+  },
   spyOn(target: any, method: string) {
     Object.defineProperty(target, method, {
       value: fn(target[method]),
