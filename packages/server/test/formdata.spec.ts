4663306aef96c9587fd158aa8220ddb501d8b66e
Denis Badurina <denis@denelop.com>
2025-04-14 15:30:26 +0200

    test form data content length smaller

--- packages/server/test/formdata.spec.ts
+++ packages/server/test/formdata.spec.ts
@@ -1,4 +1,7 @@
+import http from 'node:http';
+import NodeFormData from 'form-data';
 import { describe, expect, it } from '@jest/globals';
+import { createServerAdapter } from '../src/createServerAdapter';
 import { runTestsForEachFetchImpl } from './test-fetch.js';
 import { runTestsForEachServerImpl } from './test-server.js';
 
@@ -49,5 +52,47 @@ describe('FormData', () => {
         });
       },
     );
+
+    it('should parse form data where content-lenght is smaller than the actual data', async () => {
+      const adapter = createServerAdapter(async request => {
+        // this will throw and the server will automatically respond with a 400
+        // TODO: why? the user should have control over the response
+        await request.formData();
+        return new Response();
+      });
+      await testServer.addOnceHandler(adapter);
+
+      const formData = new NodeFormData();
+      formData.append('foo', Buffer.alloc(1000), {
+        filename: 'foo.txt',
+        filepath: '/tmp/foo.txt',
+        contentType: 'text/plain',
+      });
+
+      const url = new URL(testServer.url);
+
+      const req = http.request({
+        method: 'post',
+        hostname: url.hostname,
+        port: url.port,
+        headers: {
+          ...formData.getHeaders(),
+          'content-length': 10,
+        },
+      });
+
+      formData.pipe(req);
+
+      const res: http.IncomingMessage = await new Promise((resolve, reject) => {
+        req.on('error', err => {
+          reject(err);
+        });
+        req.on('response', res => {
+          resolve(res);
+        });
+      });
+
+      expect(res.statusCode).toBe(400);
+    });
   });
 });
