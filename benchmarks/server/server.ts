0c33347ce1364612d22b6ea86dc4b26b68663520
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-05-21 01:50:18 +0300

    Hmm

--- benchmarks/server/server.ts
+++ benchmarks/server/server.ts
@@ -11,57 +11,51 @@ import {
 
 let serverAdapter: ServerAdapter<{}, ServerAdapterBaseObject<any, any>>;
 if (process.env.SCENARIO === 'native') {
-  serverAdapter = createServerAdapter(
-    () => globalThis.Response.json({ message: `Hello, World!` }),
-    {
-      fetchAPI: {
-        fetch: globalThis.fetch,
-        Request: globalThis.Request,
-        Response: globalThis.Response,
-        Headers: globalThis.Headers,
-        FormData: globalThis.FormData,
-        ReadableStream: globalThis.ReadableStream,
-        WritableStream: globalThis.WritableStream,
-        CompressionStream: globalThis.CompressionStream,
-        TransformStream: globalThis.TransformStream,
-        Blob: globalThis.Blob,
-        File: globalThis.File,
-        crypto: globalThis.crypto,
-        btoa: globalThis.btoa,
-        TextDecoder: globalThis.TextDecoder,
-        TextEncoder: globalThis.TextEncoder,
-        URL: globalThis.URL,
-        URLSearchParams: globalThis.URLSearchParams,
-      },
+  serverAdapter = createServerAdapter(() => new globalThis.Response('Hello, World!'), {
+    fetchAPI: {
+      fetch: globalThis.fetch,
+      Request: globalThis.Request,
+      Response: globalThis.Response,
+      Headers: globalThis.Headers,
+      FormData: globalThis.FormData,
+      ReadableStream: globalThis.ReadableStream,
+      WritableStream: globalThis.WritableStream,
+      CompressionStream: globalThis.CompressionStream,
+      TransformStream: globalThis.TransformStream,
+      Blob: globalThis.Blob,
+      File: globalThis.File,
+      crypto: globalThis.crypto,
+      btoa: globalThis.btoa,
+      TextDecoder: globalThis.TextDecoder,
+      TextEncoder: globalThis.TextEncoder,
+      URL: globalThis.URL,
+      URLSearchParams: globalThis.URLSearchParams,
     },
-  );
+  });
 } else if (process.env.SCENARIO === 'undici') {
-  serverAdapter = (createServerAdapter as any)(
-    () => undici.Response.json({ message: `Hello, World!` }),
-    {
-      fetchAPI: {
-        fetch: undici.fetch,
-        Request: undici.Request,
-        Response: undici.Response,
-        Headers: undici.Headers,
-        FormData: undici.FormData,
-        ReadableStream: globalThis.ReadableStream,
-        WritableStream: globalThis.WritableStream,
-        CompressionStream: globalThis.CompressionStream,
-        TransformStream: globalThis.TransformStream,
-        Blob: BufferBlob,
-        File: undici.File,
-        crypto: globalThis.crypto,
-        btoa: globalThis.btoa,
-        TextDecoder: globalThis.TextDecoder,
-        TextEncoder: globalThis.TextEncoder,
-        URL: globalThis.URL,
-        URLSearchParams: globalThis.URLSearchParams,
-      },
+  serverAdapter = (createServerAdapter as any)(() => new undici.Response('Hello, World!'), {
+    fetchAPI: {
+      fetch: undici.fetch,
+      Request: undici.Request,
+      Response: undici.Response,
+      Headers: undici.Headers,
+      FormData: undici.FormData,
+      ReadableStream: globalThis.ReadableStream,
+      WritableStream: globalThis.WritableStream,
+      CompressionStream: globalThis.CompressionStream,
+      TransformStream: globalThis.TransformStream,
+      Blob: BufferBlob,
+      File: undici.File,
+      crypto: globalThis.crypto,
+      btoa: globalThis.btoa,
+      TextDecoder: globalThis.TextDecoder,
+      TextEncoder: globalThis.TextEncoder,
+      URL: globalThis.URL,
+      URLSearchParams: globalThis.URLSearchParams,
     },
-  );
+  });
 } else {
-  serverAdapter = createServerAdapter(() => Response.json({ message: `Hello, World!` }));
+  serverAdapter = createServerAdapter(() => new Response('Hello, World!'));
 }
 
 if (process.env.SCENARIO === 'uwebsockets') {
