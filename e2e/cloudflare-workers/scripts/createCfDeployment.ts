eeeedfcd75b067ce20c45f87ab11fa21a91240dd
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-07-01 14:06:15 +0300

    Try again

--- e2e/cloudflare-workers/scripts/createCfDeployment.ts
+++ e2e/cloudflare-workers/scripts/createCfDeployment.ts
@@ -42,7 +42,7 @@ export function createCfDeployment(
     },
     program: async () => {
       const stackName = pulumi.getStack();
-      const workerUrl = `e2e.ardatan.workers.dev/${stackName}`;
+      const workerUrl = `${stackName}.ardatan.workers.dev`;
 
       // Deploy CF script as Worker
       const workerScript = new cf.WorkersScript('worker', {
