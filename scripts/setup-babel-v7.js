4303ee058e4797ba9914bc8b98779871dbad8c4f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-07-02 14:01:48 +0300

    Fix path of package.json

--- scripts/setup-babel-v7.js
+++ scripts/setup-babel-v7.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const packageJsonStr = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8');
+const packageJsonStr = fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8');
 const packageJson = JSON.parse(packageJsonStr);
 
 for (const devDep of Object.keys(packageJson.devDependencies)) {
