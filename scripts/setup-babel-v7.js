820ded75589b8795f3925f6c07cec56570365921
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-07-02 13:51:48 +0300

    Babel v7

---
+++ scripts/setup-babel-v7.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const packageJsonStr = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8');
+const packageJson = JSON.parse(packageJsonStr);
+
+for (const devDep of Object.keys(packageJson.devDependencies)) {
+  if (devDep.startsWith('@babel/')) {
+    packageJson.devDependencies[devDep] = '^7';
+  }
+}
+
+fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(packageJson, null, 2));
